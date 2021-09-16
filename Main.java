54dc2f1227cfebf3ada1c25e695733b7fae790a3
Jason Comstock <71102533+JMComstock@users.noreply.github.com>
2021-09-16 13:58:47 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,63 @@
+package com.company;
+
+import java.io.*;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.List;
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    try(FileOutputStream binFile = new FileOutputStream("data.dat");
+            FileChannel binChannel = binFile.getChannel()) {
+
+	        ByteBuffer buffer = ByteBuffer.allocate(100);
+
+	        byte[] outputBytes = "Hello World!".getBytes();
+	        byte[] outputBytes2 = "Nice to meet you".getBytes();
+            buffer.put(outputBytes).putInt(245).putInt(-98765).put(outputBytes2).putInt(1000);
+            buffer.flip();
+
+//            buffer.putInt(245);
+//            buffer.putInt(-98765);
+//            buffer.put(outputBytes2);
+//            buffer.putInt(1000);
+
+//	          byte[] outputBytes = "Hello World!".getBytes();
+//            buffer.put(outputBytes);
+//            buffer.putInt(245);
+//            buffer.putInt(-98765);
+//            byte[] outputBytes2 = "Nice to meet you".getBytes();
+//            buffer.put(outputBytes2);
+//            buffer.putInt(1000);
+//            buffer.flip();
+
+            binChannel.write(buffer);
+
+            RandomAccessFile ra = new RandomAccessFile("data.dat", "rwd");
+            FileChannel channel = ra.getChannel();
+
+            ByteBuffer readBuffer = ByteBuffer.allocate(100);
+            channel.read(readBuffer);
+            readBuffer.flip();
+            byte[] inputString = new byte[outputBytes.length];
+            readBuffer.get(inputString);
+            System.out.println("inputString = " + new String(inputString));
+            System.out.println("int1 = " + readBuffer.getInt());
+            System.out.println("int2 = " + readBuffer.getInt());
+            byte[] inputString2 = new byte[outputBytes2.length];
+            readBuffer.get(inputString2);
+            System.out.println("inputString2 = " + new String(inputString2));
+            System.out.println("int3 = " + readBuffer.getInt());
+
+        } catch (IOException e) {
+	        e.printStackTrace();
+        }
+    }
+}
